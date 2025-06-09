5e9617aa88f94013f57161d773140bcd19b9f5d4
Vivek Deshmukh <vivek162005@gmail.com>
2025-06-09 13:08:32 +0530

    Revert "env variables fix"
    This reverts commit 50923098f1339b5e35df2b72a7746d88b70437c5.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyAgxC4IQTyGnv-Br2vp__DWXpcYe_ktNCs",
+    authDomain: "repo-scout.firebaseapp.com",
+    projectId: "repo-scout",
+    storageBucket: "repo-scout.firebasestorage.app",
+    messagingSenderId: "611685050185",
+    appId: "1:611685050185:web:348339373f6ca3afcb1c98",
+    measurementId: "G-NE69G4GP2F"
   };
 
 const app = initializeApp(firebaseConfig);
